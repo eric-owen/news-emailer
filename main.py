f7dbd501e58dbfdfe5de25787a5eca93191cd493
eric-owen <ejo.owen@icloud.com>
2023-03-23 13:06:07 -0400

    connects with API

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import requests
+
+api_key = 'c5277bbf8a444cbabab89db5e6b4fd47'
+url = 'https://newsapi.org/v2/everything?q=apple&from=2023-03-22&to=2023-03-22&sortBy=popularity&apiKey=c5277bbf8a444cbabab89db5e6b4fd47'
+
+request = requests.get(url)
+content = request.json()
+
+for article in content['articles']:
+    print(article['title'])
+    print(article['description'])
