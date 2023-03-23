23d08bd4470014a0d660c634651625c597006e38
eric-owen <ejo.owen@icloud.com>
2023-03-23 13:22:02 -0400

    connects main with email config to send email

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from email_config import send_email
 
 api_key = 'c5277bbf8a444cbabab89db5e6b4fd47'
 url = 'https://newsapi.org/v2/everything?q=apple&from=2023-03-22&to=2023-03-22&sortBy=popularity&apiKey=c5277bbf8a444cbabab89db5e6b4fd47'
@@ -6,6 +7,8 @@ url = 'https://newsapi.org/v2/everything?q=apple&from=2023-03-22&to=2023-03-22&s
 request = requests.get(url)
 content = request.json()
 
+message_body = ''
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+    message_body = message_body + article['title'] + '\n' + article['description'] + 2*'\n'
+
+send_email(message_body)
